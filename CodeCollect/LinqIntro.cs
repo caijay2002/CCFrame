f8c053aa92d7c5c7b2ab18d2a01120d976380eef
蔡程健 <caijay2002@qq.com>
2022-06-07 22:16:43 +0800

    Linq

---
+++ CodeCollect/LinqIntro.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLib;
+
+#region << 版 本 注 释 >>
+/*----------------------------------------------------------------
+ * 版权所有 (c) 2022   保留所有权利。
+ * CLR版本：4.0.30319.42000
+ * 公司名称：
+ * 命名空间：CodeCollect
+ * 文件名：LinqIntro
+ * 创建者：蔡程健
+ * 创建时间：2022/6/7 22:15:48
+ * 版本：V1.0.0
+ * 描述：
+ *
+ * ----------------------------------------------------------------
+ * 修改人：
+ * 时间：
+ * 修改说明：
+ *
+ * 版本：V1.0.1
+ *----------------------------------------------------------------*/
+#endregion << 版 本 注 释 >>
+
+namespace CodeCollect
+{
+    internal class LinqIntro
+    {
+        static void Main()
+        {
+            LINQQuery();
+            ExtensionMethods();
+            DeferredQuery();
+        }
+
+        static void DeferredQuery()
+        {
+            var names = new List<string> { "Nino", "Alberto", "Juan", "Mike", "Phil" };
+
+            var namesWithJ = from n in names
+                             where n.StartsWith("J")
+                             orderby n
+                             select n;
+
+            Console.WriteLine("First iteration");
+            foreach (string name in namesWithJ)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine();
+
+            names.Add("John");
+            names.Add("Jim");
+            names.Add("Jack");
+            names.Add("Denny");
+
+            Console.WriteLine("Second iteration");
+            foreach (string name in namesWithJ)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine();
+        }
+
+        static void ExtensionMethods()
+        {
+            var champions = new List<Racer>(Formula1.GetChampions());
+            IEnumerable<Racer> brazilChampions =
+                champions.Where(r => r.Country == "Brazil")
+                    .OrderByDescending(r => r.Wins)
+                    .Select(r => r);
+
+            foreach (Racer r in brazilChampions)
+            {
+                Console.WriteLine($"{r:A}");
+            }
+            Console.WriteLine();
+        }
+
+
+        static void LINQQuery()
+        {
+            var query = from r in Formula1.GetChampions()
+                        where r.Country == "Brazil"
+                        orderby r.Wins descending
+                        select r;
+
+            foreach (var r in query)
+            {
+                Console.WriteLine($"{r:A}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
