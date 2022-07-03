6092c54b2266a755826bd38dd165ec2e78b497a4
蔡程健 <caijay2002@qq.com>
2022-07-03 23:02:42 +0800

    addupdate

---
+++ CCFrame/Extensions/HtmlExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+#region << 版 本 注 释 >>
+/*----------------------------------------------------------------
+ * 版权所有 (c) 2022   保留所有权利。
+ * CLR版本：4.0.30319.42000
+ * 公司名称：
+ * 命名空间：CCFrame.Extensions
+ * 文件名：HtmlExtensions
+ * 创建者：蔡程健
+ * 创建时间：2022/7/3 9:28:23
+ * 版本：V1.0.0
+ * 描述：
+ *
+ * ----------------------------------------------------------------
+ * 修改人：
+ * 时间：
+ * 修改说明：
+ *
+ * 版本：V1.0.1
+ *----------------------------------------------------------------*/
+#endregion << 版 本 注 释 >>
+
+namespace CCFrame.Extensions
+{
+    internal class HtmlExtensions
+    {
+    }
+}
---
+++ CodeCollect/DataLib/BindableObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+#region << 数值发生改变时刷新界面 >>
+/*----------------------------------------------------------------
+ * 版权所有 (c) 2022   保留所有权利。
+ * CLR版本：4.0.30319.42000
+ * 公司名称：
+ * 命名空间：CodeCollect.DataLib
+ * 文件名：BindableObject
+ * 创建者：蔡程健
+ * 创建时间：2022/7/2 21:18:02
+ * 版本：V1.0.0
+ * 描述：
+ *
+ * ----------------------------------------------------------------
+ * 修改人：
+ * 时间：
+ * 修改说明：
+ *
+ * 版本：V1.0.1
+ *----------------------------------------------------------------*/
+#endregion << 版 本 注 释 >>
+
+namespace CodeCollect.DataLib
+{
+    public class BindableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        protected virtual bool SetProperty<T>(ref T item, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(item, value)) return false;
+            item = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
